4fddfef432e2159d0dc3284a5b923fef07479ce9
Brandon Koederitz <bkoederitz@mail.bradley.edu>
2025-08-28 15:20:16 -0500

    Tank Animations
    Added animations to the tank's movement and shooting

--- LaneDefender/Assets/Scripts/Player/TankMovement.cs
+++ LaneDefender/Assets/Scripts/Player/TankMovement.cs
@@ -13,6 +13,7 @@ using UnityEngine.InputSystem;
 namespace LaneDefender.Movement
 {
     [RequireComponent (typeof(PlayerInput))]
+    [RequireComponent(typeof(Rigidbody2D))]
     public class TankMovement : MonoBehaviour
     {
         #region CONSTS
@@ -21,13 +22,17 @@ namespace LaneDefender.Movement
         #endregion
 
         [SerializeField] private float lerpSpeed;
+        [Header("Animation Settings")]
+        [SerializeField, Tooltip("Reference to the graphics for this object to animate the movement")] 
+        private Transform graphicsTransform;
+        [SerializeField] private float maxAngle;
+        [SerializeField] private float smoothTime = 0.1f;
 
         // Input
         private InputAction moveAction;
 
         private Coroutine lerpCoroutine;
         private int currentLaneIndex;
-
 
         /// <summary>
         /// Setup input on awake.
@@ -40,8 +45,6 @@ namespace LaneDefender.Movement
 
                 moveAction.performed += MoveAction_Performed;
             }
-
-
         }
 
         /// <summary>
@@ -60,7 +63,11 @@ namespace LaneDefender.Movement
         private void MoveAction_Performed(InputAction.CallbackContext obj)
         {
             // Reads the direction of the player's input 
-            MoveLane(Mathf.RoundToInt(obj.ReadValue<float>()));
+            int moveValue = Mathf.RoundToInt(obj.ReadValue<float>());
+            if ( Mathf.Abs(moveValue) != 0)
+            {
+                MoveLane(moveValue);
+            }
         }
 
         /// <summary>
@@ -102,15 +109,47 @@ namespace LaneDefender.Movement
         /// <returns></returns>
         private IEnumerator LerpRoutine(Vector2 targetPosition)
         {
+            Debug.Log("Lerping");
+            // Use a vector pointing from our current position to our target position to caluclate which direction or oubject rotates in.
+            Vector2 toTargetVector = targetPosition - (Vector2)transform.position;
+            int angleDir = Mathf.RoundToInt(Mathf.Abs(toTargetVector.y) / toTargetVector.y);
+
+            float startingDist = toTargetVector.magnitude;
+            float angleSpeed = 0;
+            // Animates the moved object by rotating it's graphics object based on how much it has traveled.
+            void AnimateMovement()
+            {
+                Vector3 angles = graphicsTransform.eulerAngles;
+                // Find the normalized distance along our object's movement path we are currently at.  We'll
+                // use this to LERP our angle.
+                float normalizedDist = Vector2.Distance(transform.position, targetPosition) / startingDist;
+                //Debug.Log(normalizedDist);
+                // Calculate the target angle we should be at at this point based on our distance on the path.
+                float targetAngle = Mathf.Lerp(0, maxAngle * angleDir, normalizedDist);
+                // Smooth our angle towards our target angle.
+                angles.z = Mathf.SmoothDampAngle(angles.z, targetAngle, ref angleSpeed, smoothTime);
+                //angles.z = targetAngle;
+
+                graphicsTransform.eulerAngles = angles;
+            }
+
+
             while (Vector2.Distance(transform.position, targetPosition) > LERP_SNAP_RANGE)
             {
                 // Continually LERP our position towards our target position
                 float step = 1 - Mathf.Pow(0.5f, lerpSpeed * Time.deltaTime);
                 transform.position = Vector2.Lerp(transform.position, targetPosition, step);
+
+                AnimateMovement();
+
                 yield return null;
             }
-            // Snap to our target posittion once our LERP is done.
+            // Snap to our target posittion and rotation once our LERP is done.
             transform.position = targetPosition;
+            Vector3 angles = graphicsTransform.eulerAngles;
+            angles.z = 0;
+            graphicsTransform.eulerAngles = angles;
+
             lerpCoroutine = null;
         }
     }
